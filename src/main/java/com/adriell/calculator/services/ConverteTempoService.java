ab922b460b2016b4afc00bd9973171ec2f41b84a
Adriell Bezerra <adrielprof01@gmail.com>
2020-07-22 11:31:36 -0300

    Correção e finalização da classe ConverteTempoService

--- src/main/java/com/adriell/calculator/services/ConverteTempoService.java
+++ src/main/java/com/adriell/calculator/services/ConverteTempoService.java
@@ -6,23 +6,23 @@ import org.springframework.stereotype.Service;
 public class ConverteTempoService {
 
 	public double diaMes(double valorAD) {
-		return valorAD * 12;
+		return valorAD * 30;
 	}
 	
 	public double diaBimestre(double valorAD) {
-		return valorAD * (12*2);
+		return valorAD * (30*2);
 	}
 	
 	public double diaTrimestre(double valorAD) {
-		return valorAD * (12*3);
+		return valorAD * (30*3);
 	}
 	
 	public double diaSemestre(double valorAD) {
-		return valorAD * (12*6);
+		return valorAD * (30*6);
 	}
 	
 	public double diaAno(double valorAD) {
-		return valorAD * (12*12);
+		return valorAD * (30*12);
 	}
 	
 	public double mesDia(double valorAM) {
@@ -63,5 +63,65 @@ public class ConverteTempoService {
 	
 	public double bimestreAno(double valorAB) {
 		return bimestreMes(valorAB) * 12;
+	}
+	
+	public double trimestreDia(double valorAT) {
+		return valorAT/(30*3);
+	}
+	
+	public double trimestreMes(double valorAT) {
+		return valorAT/3;
+	}
+	
+	public double trimestreBimestre(double valorAT) {
+		return trimestreMes(valorAT) * 2;
+	}
+	
+	public double trimestreSemestre(double valorAT) {
+		return valorAT * 2;
+	}
+	
+	public double trimestreAno(double valorAT) {
+		return valorAT * 4;
+	}
+	
+	public double semestreDia(double valorAS) {
+		return valorAS/(30*6);
+	}
+	
+	public double semestreMes(double valorAS) {
+		return valorAS/6;
+	}
+	
+	public double semestreBimestre(double valorAS) {
+		return valorAS/3;
+	}
+	
+	public double semestreTrimestre(double valorAS) {
+		return valorAS/2;
+	}
+	
+	public double semestreAno(double valorAS) {
+		return valorAS * 2;
+	}
+	
+	public double anoDia(double valorAA) {
+		return valorAA/(30 * 12);
+	}
+	
+	public double anoMes(double valorAA) {
+		return valorAA/12;
+	}
+	
+	public double anoBimestre(double valorAA) {
+		return valorAA/6;
+	}
+	
+	public double anoTrimestre(double valorAA) {
+		return valorAA/4;
+	}
+	
+	public double anoSemestre(double valorAA) {
+		return valorAA/2;
 	}
 }
